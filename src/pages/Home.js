898eda12826e6ad2a6f2ad24f3e2c31f57113526
Bantale <bashorunbethel99@gmail.com>
2022-12-21 15:56:15 +0100

    resido-frontend

---
+++ src/pages/Home.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import '../App.css'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import logo from '../images/LOGO.svg'
+import circle from '../images/Ellipse 1.png'
+import instagram from '../images/image 14.png'
+
+
+const Home = () => {
+  const [email, setEmail] = useState('')
+  const url = 'http://localhost:3700/waitlist/join'
+  const redirect = useNavigate()
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const body = { email }
+    fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    }).then(() => {
+      redirect("/join")
+    })
+  }
+
+  return (
+    <div className="Home">
+      <div className="navbar">
+        <a href="">
+          <img src={logo} className="logo" alt="residoLogo" />
+        </a>
+        <img src={circle} className="circle" alt="circle" />
+      </div>
+
+      <div className="header">
+        <h1>
+          Be one of the very first to <span>Experience Resido!</span>
+        </h1>
+        <h4>
+          Experience premium home management services that <br /> eliminate the
+          added stress of maintaining your homes and facilities
+        </h4>
+        <h4>Join our exclusive waitlist today!!!</h4>
+      </div>
+
+      <div className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your Email Address"
+          className="input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type="submit" onClick= {handleSubmit}> Get early access</button>
+      </div>
+
+      <div className="policy">
+        <p>
+          By clicking “Continue” you agree to our <br />{' '}
+          <span className="privacy">Privacy Policy</span> and{' '}
+          <span className="privacy">Terms of Use</span>
+        </p>
+      </div>
+
+      <div className="social">
+        <p>Follow us on Instagram</p>
+        <a href="https://www.instagram.com/myresido_ng/">
+          <img src={instagram} className="instagram" alt="" />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Setup.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../App.css'
+import logo from '../images/LOGO.svg'
+import circle from '../images/Ellipse 1.png'
+import instagram from '../images/image 14.png'
+
+const Setup = () => {
+    console.log("Tola");
+    return (
+        <div className="Home">
+      <div className="navbar">
+        <a href="">
+          <img src={logo} className="logo" alt="residoLogo" />
+        </a>
+        <img src={circle} className="circle" alt="circle" />
+      </div>
+
+      <div className="header">
+        <h1>
+          Be one of the very first to <span>Experience Resido!</span>
+        </h1>
+        <h4>
+          Experience premium home management services that <br /> eliminate the
+          added stress of maintaining your homes and facilities
+        </h4>
+      </div>
+
+        <div>
+            <h1>Yay! Thanks for Signing up to Join <span>Resido</span> .</h1>
+        </div>
+
+      <div className="social">
+        <p>Follow us on Instagram</p>
+        <a href="https://www.instagram.com/myresido_ng/">
+          <img src={instagram} className="instagram" alt="" />
+        </a>
+      </div>
+    </div>
+    )
+}
+
+export default Setup
